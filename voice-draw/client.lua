1e15106a744649eb5780df36062dc9bc9020ad34
sheriffcounty <63911011+sh3riffcounty@users.noreply.github.com>
2021-03-18 00:04:38 +0600

    Add files via upload

---
+++ voice-draw/client.lua
@@ -0,0 +1,23 @@
+local Distance = 15
+
+Citizen.CreateThread(function()
+    while true do
+        for id = 0, 31 do
+            if  ((NetworkIsPlayerActive( id )) and GetPlayerPed( id ) ~= GetPlayerPed( -1 )) then
+                ped = GetPlayerPed( id )
+ 
+                x1, y1, z1 = table.unpack( GetEntityCoords( GetPlayerPed( -1 ), true ) )
+                x2, y2, z2 = table.unpack( GetEntityCoords( GetPlayerPed( id ), true ) )
+                distance = math.floor(GetDistanceBetweenCoords(x1,  y1,  z1,  x2,  y2,  z2,  true))
+		local takeaway = 0.95
+
+                if ((distance < Distance) and IsEntityVisible(GetPlayerPed(id))) ~= GetPlayerPed( -1 ) then
+		    if NetworkIsPlayerTalking(id) then
+			DrawMarker(25,x2,y2,z2 - takeaway, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 10.3, 55, 160, 205, 105, 0, 0, 2, 0, 0, 0, 0)
+                    end
+                end  
+            end
+        end
+        Citizen.Wait(0)
+    end
+end)
